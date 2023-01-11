11f36bf0b4e21b8f82708a26c7befbd8cbc83e38
nanobyter <sotarfredrik@gmail.com>
2023-01-11 22:00:32 +0100

    Started javascript

---
+++ todo-list/script.js
@@ -0,0 +1,13 @@
+const listsContainer = document.querySelector("[data-lists]");
+
+let lists = [];
+
+function render() {
+  clearElements(listsContainer);
+
+  lists.forEach(list => {
+    adfhasdh;
+  });
+}
+
+function clearElements(element) {}
