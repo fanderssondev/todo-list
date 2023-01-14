436fd5f5cc6a690886742901eb53e0e11b1aa7ae
nanobyter <sotarfredrik@gmail.com>
2023-01-14 18:37:28 +0100

    Fixed adding list

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,15 +1,38 @@
 const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[new-list-input]');
+const deleteListButton = document.querySelector('[data-delete-list-button]');
+
+const listDisplayContainer = document.querySelector(
+  '[data-list-display-container]'
+);
+const listTitleElement = document.querySelector('[data-list-title]');
+const listCountElement = document.querySelector('[data-list-count]');
+const tasksContainer = document.querySelector('[data-tasks]');
 
 const LOCAL_STORAGE_LIST_KEY = 'task.list';
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
-let selectedListId =
-  JSON.parse(localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)) || [];
+let selectedListId = JSON?.parse(
+  localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+);
 
-newListForm.addEventListener('submit', event => {
-  event.preventDefault();
+listsContainer.addEventListener('click', e => {
+  if (e.target.tagName.toLowerCase() === 'li') {
+    selectedListId = e.target.dataset.listId;
+    saveAndRender();
+  }
+});
+
+deleteListButton.addEventListener('click', () => {
+  lists = lists.filter(list => list.id !== selectedListId);
+  selectedListId = null;
+  saveAndRender();
+  console.log('delete');
+});
+
+newListForm.addEventListener('submit', e => {
+  e.preventDefault();
 
   const listName = newListInput.value;
   if (listName == null || listName === '') return;
@@ -21,7 +44,13 @@ newListForm.addEventListener('submit', event => {
 });
 
 function createList(listName) {
-  return { id: (lists.length + 1).toString(), name: listName, tasks: [] };
+  const newList = {
+    id: (lists.length + 1).toString(),
+    name: listName,
+    tasks: [],
+  };
+  selectedListId = newList.id;
+  return newList;
 }
 
 function saveAndRender() {
@@ -31,16 +60,42 @@ function saveAndRender() {
 
 function save() {
   localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
 }
 
 function render() {
   clearElements(listsContainer);
+  renderLists();
 
+  const selectedList = lists.find(list => list.id === selectedListId);
+  if (selectedListId == null) {
+    listDisplayContainer.style.display = 'none';
+  } else {
+    listDisplayContainer.style.display = '';
+    console.log(selectedList.name);
+    listTitleElement.innerText = selectedList.name;
+    renderTaskCount(selectedList);
+    clearElements(tasksContainer);
+  }
+}
+
+function renderTaskCount(selectedList) {
+  const incompleteTasksCount = selectedList.tasks.filter(
+    task => task.complete == false
+  ).length;
+  const taskString = incompleteTasksCount === 1 ? 'task' : 'tasks';
+  listCountElement.innerText = `${incompleteTasksCount} ${taskString} remaining`;
+}
+
+function renderLists() {
   lists.forEach(list => {
     const listElement = document.createElement('li');
     listElement.dataset.listId = list.id;
     listElement.classList.add('list-name');
     listElement.innerText = list.name;
+    if (list.id == selectedListId) {
+      listElement.classList.add('active-list');
+    }
     listsContainer.appendChild(listElement);
   });
 }
