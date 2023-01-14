be6f6ed6ac4c496219422cd808a7da75c84d3e0c
nanobyter <sotarfredrik@gmail.com>
2023-01-14 17:05:16 +0100

    Can add a list, and fixed local storage

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,13 +1,54 @@
-const listsContainer = document.querySelector("[data-lists]");
+const listsContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[new-list-input]');
 
-let lists = [];
+const LOCAL_STORAGE_LIST_KEY = 'task.list';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId =
+  JSON.parse(localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)) || [];
+
+newListForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const listName = newListInput.value;
+  if (listName == null || listName === '') return;
+
+  const list = createList(listName);
+  newListInput.value = null;
+  lists.push(list);
+  saveAndRender();
+});
+
+function createList(listName) {
+  return { id: (lists.length + 1).toString(), name: listName, tasks: [] };
+}
+
+function saveAndRender() {
+  save();
+  render();
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+}
 
 function render() {
   clearElements(listsContainer);
 
   lists.forEach(list => {
-    adfhasdh;
+    const listElement = document.createElement('li');
+    listElement.dataset.listId = list.id;
+    listElement.classList.add('list-name');
+    listElement.innerText = list.name;
+    listsContainer.appendChild(listElement);
   });
 }
 
-function clearElements(element) {}
+function clearElements(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+render();
